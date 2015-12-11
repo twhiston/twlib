494880ad4b1cd14a79950fec86a9fa32149dd286
Tom W <tom.whiston@gmail.com>
2015-12-12 00:51:24 +0100

    added enum class based on well known online example

---
+++ twLib_Enum.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * Part of twLib
+ * http://www.thomaswhiston.com
+ */
+namespace twLib;
+
+/**
+ * Created by PhpStorm.
+ * User: Thomas Whiston
+ * Date: 12/12/2015
+ * Time: 00:47
+ *
+ *   abstract class DaysOfWeek extends twLib\Enum {
+ *     const Sunday = 0;
+ *     const Monday = 1;
+ *     const Tuesday = 2;
+ *     const Wednesday = 3;
+ *     const Thursday = 4;
+ *     const Friday = 5;
+ *     const Saturday = 6;
+ *   }
+ */
+
+abstract class Enum {
+
+  private static $constCacheArray = NULL;
+
+  private function __construct(){
+    /*
+      Preventing instantiation
+    */
+  }
+
+  private static function getConstants() {
+    if (self::$constCacheArray == NULL) {
+      self::$constCacheArray = [];
+    }
+    $calledClass = get_called_class();
+    if (!array_key_exists($calledClass, self::$constCacheArray)) {
+      $reflect = new \ReflectionClass($calledClass);
+      self::$constCacheArray[$calledClass] = $reflect->getConstants();
+    }
+    return self::$constCacheArray[$calledClass];
+  }
+
+  public static function isValidName($name, $strict = false) {
+    $constants = self::getConstants();
+
+    if ($strict) {
+      return array_key_exists($name, $constants);
+    }
+
+    $keys = array_map('strtolower', array_keys($constants));
+    return in_array(strtolower($name), $keys);
+  }
+
+  public static function isValidValue($value) {
+    $values = array_values(self::getConstants());
+    return in_array($value, $values, $strict = true);
+  }
+}
+
+
